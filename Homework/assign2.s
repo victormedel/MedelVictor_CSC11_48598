b20c5ac86a6f72912c569847987aff842f25d669
Victor Medel <victor.medel@roadrunner.com>
2014-09-19 20:26:54 -0700

    Update assign2.s

--- Homework/assign2.s
+++ Homework/assign2.s
@@ -6,11 +6,13 @@ _start:
 	MOV R3, #5		@ b
 
 	SUBS R1, R2, R3		@ R1=R2-R3
+	CMP R1, R3		@ Compare R1 with R3
+	BLT stop		@ If R1 is less than R3 then go to stop
 
 loop:
 	SUBS R1, R1, R3		@ R1=R1-R3	
 	CMP R1, R3		@ Compare R1 with R3
-	BLT stop		@ If R1<R3
+	BAL loop		@ branch back to loop if R1 is not < R3
  
 stop:
 	
