cb1e2d3460f5ea76a97d90ad5139cd4d42fec9d6
Victor Medel <victor.medel@roadrunner.com>
2014-09-19 21:21:35 -0700

    Update assign2.s

--- Homework/assign2.s
+++ Homework/assign2.s
@@ -2,17 +2,22 @@
 
 	.global _start
 _start:
-	MOV R2, #20		@ a
-	MOV R3, #5		@ b
+	MOV R2, #20		@ a=20
+	MOV R3, #5		@ b=5
 
 	SUBS R1, R2, R3		@ R1=R2-R3
 	CMP R1, R3		@ Compare R1 with R3
-	BLT stop		@ If R1 is less than R3 then go to stop
+	BLT _exit		@ If R1 is less than R3 then exit
+	BAL _loop		@ otherwise go to loop
 
-loop:
+_loop:
 	SUBS R1, R1, R3		@ R1=R1-R3	
 	CMP R1, R3		@ Compare R1 with R3
-	BAL loop		@ branch back to loop if R1 is not < R3
+	BLT _exit		@ If R1 is less than R3 then exit
+	BAL _loop		@ otherwise go back to the start of the loop
+	
  
-stop:
+_exit:
+	MOV R7, #1
+	SWI 0
 	
