48c336ac1e509cb11e6172007310beec22aef832
Victor Medel <victor.medel@roadrunner.com>
2014-09-28 09:33:20 -0700

    Update assign3b.s

--- Homework/assign3b.s
+++ Homework/assign3b.s
@@ -15,30 +15,28 @@
  
      MOV R2, #111          @ a
      MOV R3, #5            @ b
-     MOV R4, #1            @ Used to swap R0 and R1 "flag IS set"
+     MOV R4, #1            @ Used to swap R0 and R1 "flag is NOT set"
      MOV R0, #0            @ Set Counter to 0
-     MOV R1, R2                @ Set R1=R2
+     MOV R1, R2            @ Set R1=R2
  
- /*   Test to make sure that R1<R3   */
+ /*   Test if R1>=R3   */
 
-     CMP R1, R3             @ Compare R1 with R3
-     BLT flag              @ If R1 is less than R3 go to flag
+     CMP R1, R3            @ Compare R1 with R3
+     BGE loop              @ If R1 is greater than or equal to R3 go to loop
     
  loop:
      SUBS R1, R1, R3        @ R1=R1-R3
      ADDS R0, #1            @ Add 1 to the counter
      CMP R1, R3             @ Compare R1 with R3
-     BLT flag              @ If R1 is less than R3 go to flag
-     BAL loop              @ Otherwise go back to the start of the loop
+     BGE loop               @ If R1 is greater than or equal to R3 go to loop
+     BAL flag               @ Otherwise go to flag
      
  flag:
      CMP R4, #0             @ Check if flag is set
-     BEQ exit              @ If flag is set to 0 it will exit
+     BEQ exit               @ If flag is set to 0 it will exit
                             @ Otherwise it will swap registers
-     MOV R4, R1             @ R1 goes to R4
      MOV R5, R0             @ R0 goes to R5
-     MOV R0, R4             @ R0 is assigned R4 value
-     
+     MOV R0, R1             @ R1 goes to R0
      MOV R1, R5             @ R1 is assigned R5 value
      
  exit:
