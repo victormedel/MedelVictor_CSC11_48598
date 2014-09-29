c1b6065c01327a51775d363b1d6e9677cda763af
Victor Medel <victor.medel@roadrunner.com>
2014-09-28 19:20:02 -0700

    Update assign3d.s

--- Homework/assign3d.s
+++ Homework/assign3d.s
@@ -10,7 +10,7 @@
      .global main
  main:
  
-     MOV R2, #22222        @ a
+     LDR R2, =22222        @ a
      MOV R3, #5            @ b
      MOV R4, #1            @ Used to swap R0 and R1 "flag IS set"
      MOV R5, #0		          @ Sets Register R5 to 0
