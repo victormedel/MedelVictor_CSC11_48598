acb97fcdd6010500c7c1ca7c44ad5e3cda45710a
Victor Medel <victor.medel@roadrunner.com>
2014-09-29 10:47:37 -0700

    Update assign3c.s

--- Homework/assign3c.s
+++ Homework/assign3c.s
@@ -20,7 +20,7 @@
      MOV R9, #0		          @ Sets Register R9 to 0
      MOV R10, #0           @ Sets Register R10 to 0
      MOV R0, #0            @ Set Counter to 0
-     MOV R1, R2            @ Set R1=R2
+     MOVS R1, R2            @ Set R1=R2
  
  /*   Test if R1>=R3   */
 
@@ -40,7 +40,7 @@
      BAL loop               @ Otherwise jump to loop
      
  inscale:
-     MOV R10, R6            @ R10=R6
+     MOVS R10, R6            @ R10=R6
      MULS R6, R10, R8       @ R6=R6*R8
      MULS R7, R3, R6        @ R7=R3*R6
      MULS R9, R7, R8        @ R9=R7*R8
@@ -59,9 +59,9 @@
      CMP R4, #0             @ Check if flag is set
      BEQ exit               @ If flag is set to 0 it will exit
                             @ Otherwise it will swap registers
-    MOV R5, R0              @ R0 goes to R5
-    MOV R0, R1              @ R1 goes to R0
-    MOV R1, R5              @ R1 is assigned R5 value
+    MOVS R5, R0              @ R0 goes to R5
+    MOVS R0, R1              @ R1 goes to R0
+    MOVS R1, R5              @ R1 is assigned R5 value
      
  exit:
      MOV R7, #1
