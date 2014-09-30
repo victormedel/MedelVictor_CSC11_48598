bf2a9cec538606bb67f788c73ecfad28753d7fcc
Victor Medel <victor.medel@roadrunner.com>
2014-09-29 20:45:37 -0700

    Update assign3c.s

--- Homework/assign3c.s
+++ Homework/assign3c.s
@@ -1,16 +1,15 @@
-/* 
- * 
- * Author: Victor Medel
- * Created On: September 27 ,2014
- * Purpose: More Efficient Technique for Calculating a/b and a%b
- * Assignment 3
- *
- */
+
+
+@ Author: Victor Medel
+@ Created On: September 27 ,2014
+@ Purpose: More Efficient Technique for Calculating a/b and a%b
+@ Assignment 3
+
  
      .global main
  main:
  
-     LDR R2, =100       @ a
+     LDR R2, =0x56CE       @ a
      MOV R3, #5            @ b
      MOV R4, #0            @ Used to swap R0 and R1 "flag is NOT set"
      MOV R5, #0		          @ Sets Register R5 to 0
