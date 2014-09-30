e58d8cac5166c5f132d7d49a7970ce95055341c0
Victor Medel <victor.medel@roadrunner.com>
2014-09-29 21:36:57 -0700

    Update assign3c.s

--- Homework/assign3c.s
+++ Homework/assign3c.s
@@ -28,9 +28,9 @@
      BAL flag               @ Otherwise jump to flag
      
  scale:
-     MOVS R6, #1            @ R6=1 scale
-     MULS R7, R3, R6        @ R7=R3*R6 subtraction factor
-     MULS R9, R7, R8        @ R9=R7*R8 next subtraction factor to test
+     MOV R6, #1            @ R6=1 scale
+     MUL R7, R3, R6        @ R7=R3*R6 subtraction factor
+     MUL R9, R7, R8        @ R9=R7*R8 next subtraction factor to test
      BAL scalecomp
 
  scalecomp:    
@@ -39,15 +39,15 @@
      BAL loop               @ Otherwise jump to loop
      
  inscale:
-     MOVS R10, R6           @ R10=R6
-     MULS R6, R10, R8       @ R6=R6*R8
-     MULS R7, R3, R6        @ R7=R3*R6
-     MULS R9, R7, R8        @ R9=R7*R8
+     MOV R10, R6           @ R10=R6
+     MUL R6, R10, R8       @ R6=R6*R8
+     MUL R7, R3, R6        @ R7=R3*R6
+     MUL R9, R7, R8        @ R9=R7*R8
      BAL scalecomp
     
  loop:
      ADCS R0, R0, R6        @ R0=R0+R6 Increase by scale
-     SBCS R1, R1, R7        @ R1=R1-R7 Subtract by scale
+     SUBS R1, R1, R7        @ R1=R1-R7 Subtract by scale
      CMP R1, R7             @ Compare R1 to R7
      BGE loop               @ If R1 is greater than or Equal to R7 go back to loop
      BAL comploop
