849f426a5a628ba2456fec82288989440da44c3a
Victor Medel <victor.medel@roadrunner.com>
2014-09-29 17:52:15 -0700

    Update assign3d.s

--- Homework/assign3d.s
+++ Homework/assign3d.s
@@ -18,50 +18,53 @@
      MOV R7, #0		          @ Sets Register R7 to 0
      MOV R8, #10	          @ Sets Register R8 to 10
      MOV R9, #0		          @ Sets Register R9 to 0
+     MOV R10, #0           @ Sets Register R10 to 0
      MOV R0, #0            @ Set Counter to 0
-     MOV R1, R2            @ Set R1=R2
+     MOVS R1, R2           @ Set R1=R2
  
  /*   Test if R1>=R3   */
 
-     CMP R1, R3            @ Compare R1 with R3
-     BGE scale             @ If R1 is greater than or equal to R3 go to scale
-     BAL flag              @ Otherwise jump to flag
+     CMP R1, R3             @ Compare R1 with R3
+     BGE scale              @ If R1 is greater than or equal to R3 go to scale
+     BAL flag               @ Otherwise jump to flag
      
  scale:
-     MOV R6, #1             @ R6=1 scale
+     MOVS R6, #1            @ R6=1 scale
      MULS R7, R3, R6        @ R7=R3*R6 subtraction factor
      MULS R9, R7, R8        @ R9=R7*R8 next subtraction factor to test
-     
- scalecomp:
-     CMP R1, R9             @ Compare R1 and R9
-     BGT inscale            @ If R1 is greater than R9 go to inscale
-     BAL loop               @ Otherwise jumps to loop
+     BAL scalecomp
+
+ scalecomp:    
+     CMP R1, R9             @ Compares R1 with R9
+     BGT inscale            @ If R1 is greater than R9 then go to inscale
+     BAL loop               @ Otherwise jump to loop
      
  inscale:
-     MOV R10, R6            @ R10=R6
+     MOVS R10, R6           @ R10=R6
      MULS R6, R10, R8       @ R6=R6*R8
      MULS R7, R3, R6        @ R7=R3*R6
      MULS R9, R7, R8        @ R9=R7*R8
-     BAL scalecomp          @ Back to inscale
+     BAL scalecomp
     
  loop:
      ADDS R0, R0, R6        @ R0=R0+R6 Increase by scale
      SUBS R1, R1, R7        @ R1=R1-R7 Subtract by scale
      CMP R1, R7             @ Compare R1 to R7
      BGE loop               @ If R1 is greater than or Equal to R7 go back to loop
+     BAL comploop
+ 
+ comploop:
      CMP R6, #1             @ Compare R6 to 1
      BGT scale              @ If R6 is greater than 1 jump back to scale
-     BLT flag               @ Otherwise go to flag
+     BAL flag               @ Otherwise go to flag
      
  flag:
      CMP R4, #0             @ Check if flag is set
      BEQ exit               @ If flag is set to 0 it will exit
                             @ Otherwise it will swap registers
-    MOV R5, R0              @ R0 goes to R5
-    MOV R0, R1              @ R1 goes to R0
-    MOV R1, R5              @ R1 is assigned R5 value
+    MOVS R5, R0             @ R0 goes to R5
+    MOVS R0, R1             @ R1 goes to R0
+    MOVS R1, R5             @ R1 is assigned R5 value
      
  exit:
-     MOV R7, #1
-     SWI 0
-
+     BX LR
