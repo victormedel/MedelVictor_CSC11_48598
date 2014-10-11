b030aef17609da4fc0d4aabc6817d03420515aaa
victormedel <victor.medel@roadrunner.com>
2014-10-11 15:40:57 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

---
+++ Homework/assign4a.s
@@ -0,0 +1,55 @@
+/*
+ * Author: Victor Medel
+ * Created On: October 10, 2014
+ * Purpose: Assignment 4
+ *
+ */
+ 
+ .data
+ 
+ /* Numerator Input */
+ .balign 4
+ message1: .asciz "Please type the numerator: "
+ 
+ /* Numerator Readback */
+ .balign 4
+ message2: .asciz "I read the number %d\n"
+ 
+  /* Denominator Input */
+ .balign 4
+ message3: .asciz "Please type the denominator: "
+ 
+ /* Denominator Readback */
+ .balign 4
+ message4: .asciz "I read the number %d\n"
+ 
+ /* Format pattern for scanf */
+ .balign 4
+ scan_patter: .asciz "%d"
+ 
+ /* Where scanf will store the number read */
+ .balign 4
+ number_read: .word 0
+ 
+ .balign 4
+ return: .word 0
+ 
+ 
+ .text
+ 
+	.global main
+ main:
+ 
+ ldr r1, address_or_return			/* r1 = address_of_return */
+ str lr, [r1]						/* lr = r1 */
+ 
+ 
+ 
+ address_of_message1: .word message1
+ address_of_message2: .word message2
+ address_of_scan_patter: .word scan_pattern
+ address_of_number_read: .word number_read
+ address_of_return: .word return
+ 
+ .global printf
+ .global scanf
