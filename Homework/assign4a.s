23a2be1f313dab08c25f4aa9bbe840057fbd69c6
victormedel <victor.medel@roadrunner.com>
2014-10-15 13:42:18 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

--- Homework/assign4a.s
+++ Homework/assign4a.s
@@ -46,7 +46,6 @@
  
     .global main
  main:
- 
  	ldr r1, address_of_return					@ r1 <- address_of_return
  	str lr, [r1]					 			@ *r1 <- lr
 	
@@ -60,8 +59,8 @@
  	ldr r1, address_of_numerator_read			@ r1 <- numerator_read
 	bl scanf				         			@ call to scanf
 	
-	ldr r2, address_of_numerator_read    		@ r2 <- numerator_read
-	ldr r2, [r2]                         		@ r2 <- *r2
+	ldr r0, address_of_numerator_read    		@ r2 <- numerator_read
+	ldr r2, [r0]                         		@ r2 <- *r2
  	
 @ Denominator Input and Format
  	
@@ -72,8 +71,8 @@
  	ldr r1, address_of_denominator_read		 	@ r1 <- denomintator_read
  	bl scanf				        		 	@ call to scanf
  	
-	ldr r3, address_of_denominator_read  		@ r3 <- denominator_read
-	ldr r3, [r3]                         		@ r3 <- *r3
+	ldr r0, address_of_denominator_read  		@ r3 <- denominator_read
+	ldr r3, [r0]                         		@ r3 <- *r3
 	
 	bl division                          		@ Branchout to Division Funtion
   
@@ -88,7 +87,7 @@
 	mov r5, r3                            		@ r5 <- r3
 	ldr r3, address_of_denominator_read   		@ r3 <- denominator_read
 	ldr r3, [r3]                          		@ r3 <- *r3
-	ldr r1, address_of_message4           		@ r1 <- message4
+	ldr r0, address_of_message4           		@ r1 <- message4
 	
 	bl printf									@ return from main using lr
  
@@ -102,40 +101,35 @@
  division:
 	ldr r1, address_of_return2           	@ r1 <- address_of_return
 	str lr, [r1]				  			@ *r1 <- lr
+											
+											@ Register Initilization
+	mov r0, #0								@ r0=0
+	mov r4, #1								@ r4=1, Counter initialized
+	mov r5, r3								@ r5=r3, Set r5 equal to denominator
+	mov r1, r2								@ r1=r2, Set r1 equal to numerator
 	
+	cmp r1, r3				  				@ Compare r1 to r3
+	ble exit								@ If r1 is less than or equal to r3 exit
+	bal scaleleft							@ Otherwise continue to scaleleft
 	
-	
-	mov r1, r2								@ r1 = r2
-	cmp r1, r3				  				@ compare r1 to r3
-	bge prescaleleft				  		@ If r1 is greater than or equal to r3 jump to scale left
-	
-	
-address_of_return2: .word return2	
- 
-prescaleleft:
-	mov r4, #1 								@ r4 = 1 
-	mov r5, r3								@ r5 = r3
-	bal scaleleft
 	
 scaleleft:	
-	mov r4, r4, lsl #1						@ Scale factor
-	mov r5, r5, lsl #1						@ Subtraction factor
+	mov r4, r4, lsl #1						@ Scale factor|Division counter 
+	mov r5, r5, lsl #1						@ Subtraction|Mod/Remainder subtraction
 	cmp r1, r5								@ Compare r1 with r5
 	bge scaleleft							@ If r1 is greater than or equal to r5 loop to scaleleft
 	mov r4, r4, asr #1						@ Otherwise Scale factor back
 	mov r5, r5, asr #1 						@ and scale subtraction factor back
-	bal addsubcomp							@ Continue division function by branching to addsub	
+	bal addsub								@ Continue to addsub
 
 addsub:
-	mov r0, #0								@ r0 = 0
 	add r0, r0, r4							@ Count the subtracted scale factor
 	sub r1, r1, r5							@ Subtract the scaled mod
-	bal scaleright							@ Keep scaling right until less than remainder
+	bal scaleright							@ Continue to scaleright
 	
 addsubcomp:	
 	cmp r4, #1 								@ Compare if r4 is greater than 1
-	bgt addsub								@ If r4 greater than 1 branch to addsub
-	bx lr									@ Otherwise exit
+	bgt addsub								@ If r4 greater than 1 branch back to addsub
 
 scaleright:
 	mov r4, r4, asr #1 						@ Division Counter
@@ -145,7 +139,11 @@ scaleright:
 	bal addsubcomp							@ Otherwise go to addsubcomp
 	
 exit:
-	
+	ldr lr, address_of_return2
+	ldr lr, [lr]
+	bx lr
+	address_of_return2: .word return2	
+ 
  
  
  address_of_message1: .word message1
