c937ab7c3297993966f7c1c53cecfbc420867bf4
victormedel <victor.medel@roadrunner.com>
2014-11-05 21:02:31 -0800

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

--- Homework/timing.s
+++ Homework/timing.s
@@ -7,18 +7,18 @@
  
 .data
 message: .asciz "Start Time: %d\nEnd Time: %d\nDiffrence: %d\n"
-starts: .word 0
+begin: .word 0
 
 
 .text
 
-	.global starts
-starts:
+	.global begin
+begin:
 	push {lr}
 	
 	mov r0, #0						/* Set time to zero */
 	bl time
-	ldr r2, address_of_start		/* Set address of start as the third parameter of start */
+	ldr r2, address_of_begin		/* Set address of start as the third parameter of begin */
 	str r0, [r2]					/* Store start time in r2 */
 	
 	pop {lr}
@@ -26,26 +26,26 @@ starts:
 
 
 
-	.global ends
-ends:
+	.global ending
+ending:
 	push {lr}
 	
 	mov r0, #0						/* Set time to zero */
 	bl time
 	
 	mov r2, r0						/* Move r0/end time to r2 */
-	ldr r1, address_of_start		/* Set start address the second parameter of ends */
+	ldr r1, address_of_begin		/* Set start address the second parameter of ending */
 	ldr r1, [r1]					/* Load the start time to r1 */
 	sub r3, r2, r1					/* r3 = end time - start time
 	
-	ldr r0, address_of_message			/* Load answer to r0 */
-	bl printf						/* Print message with start, ends, and diffrece of time
+	ldr r0, address_of_message		/* Load answer to r0 */
+	bl printf						/* Print message with start, ending, and diffrece of time
 	
 	pop {lr}
 	bx lr
 	
 address_of_message: .word message
-address_of_start: .word starts
+address_of_begin: .word begin
 	
 	.global time
 	.global printf
